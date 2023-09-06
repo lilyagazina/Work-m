1300defbc13e00db9b9c248d0d40ae5077459127
Лилиана <lilya_gazina@mail.ru>
2023-09-06 22:05:29 +0300

    исправления

--- src/main/java/ru/skypro/teamwork/configuration/TelegramBotConfiguration.java
+++ src/main/java/ru/skypro/teamwork/configuration/TelegramBotConfiguration.java
@@ -11,6 +11,10 @@ public class TelegramBotConfiguration {
 
     @Value("${telegram.bot.token}")
     private String token;
-
-
+    @Bean
+    public TelegramBot telegramBot() {
+        TelegramBot bot = new TelegramBot(token);
+        bot.execute(new DeleteMyCommands());
+        return bot;
+    }
 }
