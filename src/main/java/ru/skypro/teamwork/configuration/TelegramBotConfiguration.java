e59d6fa682ec8e08de1f9d1faf8c7f8a3561bd4f
lilyagazina <120730696+lilyagazina@users.noreply.github.com>
2023-09-06 22:01:42 +0300

    Update TelegramBotConfiguration.java

--- src/main/java/ru/skypro/teamwork/configuration/TelegramBotConfiguration.java
+++ src/main/java/ru/skypro/teamwork/configuration/TelegramBotConfiguration.java
@@ -12,10 +12,5 @@ public class TelegramBotConfiguration {
     @Value("${telegram.bot.token}")
     private String token;
 
-    @Bean
-    public TelegramBot telegramBot() {
-        TelegramBot bot = new TelegramBot(token);
-        bot.execute(new DeleteMyCommands());
-        return bot;
-    }
+
 }
